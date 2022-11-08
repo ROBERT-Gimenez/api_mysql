77968db2288ab6861da2a2d592bd28d08ccd3d9e
robert giemenz <you@example.com>
2022-11-07 22:57:43 -0300

    product controller and product router added

--- controllers/api/productController.js
+++ controllers/api/productController.js
@@ -1,4 +1,7 @@
-const {products} = require("../../db");
+const {products , categori } = require("../../db");
+const {validationResult} = require("express-validator");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
     list: (req, res) => {
@@ -20,10 +23,71 @@ module.exports = {
     detalle:async (req , res) => {
         try{
             let producto = await products.findOne({where:{id:+req.params.id}},{include: ['category']} );
-
+            let category = await categori.findOne({where:{id:producto.categoryid}});
+            producto.categoryid = category.nombre ;
             return res.status(200).json(producto);
 
         }catch(error){res.send(error)}
     },
+    create:async(req , res) => {
+        try{
+            await products.create(req.body)
+            .then((product) => { res.status(200).json(product)})
+        }catch(err){res.send(err)}
+    },
+    productUpdate:async(req,res) =>{
+        try{
+            if(req.file != undefined){
+                await products.findByPk(+req.params.id)
+                    .then((Producto)=>{
+                    let image = Producto.image;
+                    try{ 
+                        if(fs.existsSync(path.join(__dirname ,'../../public/img/products/'+ image))){
+                            fs.unlinkSync(path.join(__dirname ,'../../public/img/products/'+ image))
+                        }}catch(err){ res.send(err)           
+                        }                              
+                }).catch((error)=>{ res.send(error)})
+                    
+        }
+            let errors = validationResult(req);
+            console.log(errors)
+            if(errors.isEmpty()){
+                try{
+                    let product =  await products.update({//el metodo update retorna la cantidad de elementos modificados ("1") 
+                        name: req.body.name,
+                        price: req.body.price,
+                        categoryid: req.body.categoryid,
+                        description: req.body.description,
+                        marca: req.body.marca,
+                        discount: req.body.discount,
+                        stock: req.body.stock,
+                        image: req.file? req.file.filename : this.image,
+                },{ where: { id: req.params.id}     });
+                let productUpdate = await products.findByPk(req.params.id);//por ello solicitamos el elementos modificado para enviarlo
+                   return res.status(200).json(productUpdate)
+                }catch(err){console.log(err)}
+            }
+    }catch(err){console.log(err)}
+},
+    delete:async(req,res) => {
+        try {
+            let productId = +req.params.id;
+            let product = await products.findByPk(productId);
+            if(fs.existsSync(path.join(__dirname, '../../public/img/products/' + product.image)) 
+            && product.image !== "product-default-4.png") {
+                fs.unlinkSync(path.join(__dirname, '../../public/img/products/' + product.image))
+                let productDelete = await products.destroy({ where: { id: productId } });
+            
+            }else {
 
+                let productDelete = await products.destroy({ where: { id: productId } });
+            }
+
+            res.send("eliminado")
+
+        } catch (error) {
+            res.send(error)
+        }
+    },
 }
+
--- routes/api/productApi.js
+++ routes/api/productApi.js
@@ -2,7 +2,14 @@ const router =require("express").Router();
 const productController = require("../../controllers/api/productController")
 
 router.get('/', productController.list);
+
 router.get('/:id', productController.detalle);
+
+router.post('/create', productController.create);
+
+router.put('/:id', productController.productUpdate);
+
+router.delete('/:id', productController.delete);
 
 
 module.exports = router;
