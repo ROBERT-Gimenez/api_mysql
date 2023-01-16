d54f44becba73460553846eafb86d7e7a9d46fcb
ROBERT-Gimenez <robertgim37@gmail.com>
2023-01-15 23:53:57 -0300

    edit rute

--- middlewares/imgProfile.js
+++ middlewares/imgProfile.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-    destination : ( req , file , cd) => {
+    destination : ( req , file , cb) => {
         cb(null, path.join(__dirname, '../public/images/profile'))
     },
     filename:(req,file,cb) => {
